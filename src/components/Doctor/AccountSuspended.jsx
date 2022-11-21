d2fdcd3d9c63908f1de5440c88d7d4eaa06911fe
archis <vazearchis@gmail.com>
2022-11-21 13:57:21 +0530

    spelling fix

--- src/components/Doctor/AccountSuspended.jsx
+++ src/components/Doctor/AccountSuspended.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function AccountSuspended() {
   return (
     <div className='page'>
-        <h1>Your Accound has been suspended by the Admin. Please contact Admin to recover your account.</h1>
+        <h1>Your Account has been suspended by the Admin. Please contact Admin to recover your account.</h1>
     </div>
   )
 }
